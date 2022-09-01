d30752bfe0de9d79a693a973b4bd1d76760ec525
Matamando Kalilani <matamandokalilani@outlook.com>
2022-09-01 15:48:25 +0200

    Start Save

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const colors = require("colors");
+const { graphqlHTTP } = require("express-graphql");
+const express = require("express");
+const schema = require("./schema/schema");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const port = process.env.PORT || 5000;
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server Listening on: ${port}`));
